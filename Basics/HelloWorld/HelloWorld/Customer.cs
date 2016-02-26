6c41b842a288e07bf60f831f8eaa676beb0606fd
Jyothi Babu Araja <jyothibabu.araja@apxor.com>
2016-02-27 00:40:07 +0530

    first commit for my face in C# finally

---
+++ Basics/HelloWorld/HelloWorld/Customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Customer
+    {
+
+        int customerId;
+        string name;
+        DateTime dob;
+        string phone;
+
+        public Customer(int customerId, string name, DateTime dob, string phone)
+        {
+            this.customerId = customerId;
+            this.name = name;
+            this.dob = dob;
+            this.phone = phone;
+        }
+
+        public int CustomerId
+        {
+            get
+            {
+                return this.customerId;
+            }
+            private set
+            {
+                this.customerId = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+        public double Age
+        {
+            get
+            {
+                return DateTime.Today.Subtract(this.dob).TotalDays;
+            }
+        }
+        public string Phone
+        {
+            get
+            {
+                return this.phone;
+            }
+            private set
+            {
+                this.phone = value;
+            }
+        }
+    }
+}
---
+++ Basics/HelloWorld/HelloWorld/Tester.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HelloWorld;
+
+namespace HelloWorld
+{
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Araja Jyothi Babu testing this...");
+            Customer customer = new Customer(56, "Araja Jyothi Babu", new DateTime(1994,10,23), "9533162006");
+            Console.Write("Customer Details: \n");
+            Console.Write(customer.CustomerId + "\n" + customer.Name + "\n" + customer.Age + "\n" + customer.Phone);
+            Console.Write("\nAge of Customer: " + customer.Age/365.0 + " years");
+            Console.ReadLine();
+        }
+    }
+}
